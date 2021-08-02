681600a18d391e2f691d7c0611fa18bbd1f05dac
Anais Del campo <adelcampo@cnm.edu>
2021-08-02 09:02:33 -0600

    1 added

---
+++ snap-events/script.js
@@ -0,0 +1,7 @@
+
+function handleClick() {
+    const element = document.getElementById("target")
+    element.innerHTML = "I have been clicked"
+    //remember you can set css styling using HINT HINT!
+    // element.style
+}
